659f281d3700fb900042cd46bdaaab1dfa73114c
wizardywizard <littledavid175@gmail.com>
2020-01-28 18:53:54 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, suprise){
+    let gift = []
+    for (let i = 0; i < name.length; i++) {
+        gift.push(`Thank you, ${name[i]}, for the wonderful ${suprise} gift!`);
+    }
+    return gift;
+}
+ 
+function countDown(num){
+    while (num >= 0){
+        console.log(num);
+        num--;
+    }
+    return num
+}
